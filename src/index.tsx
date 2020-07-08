29dd1cb601fc94dc2fea69aa54140b50ad5c58ae
cplus98 <40209639+cplus98@users.noreply.github.com>
2020-07-09 00:52:38 +0900

    Disconnect method added

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
-import React, { useState, useEffect, useContext, useMemo, useCallback } from 'react';
+import _ from 'lodash';
+import React, { useCallback, useContext, useEffect, useState } from 'react';
 import io from 'socket.io-client';
 
 interface SocketList {
@@ -9,23 +10,39 @@ interface SocketIOProviderProps {
 	children?: React.ReactNode | React.ReactNode[];
 }
 
-const SocketIOContext = React.createContext({});
+type SocketIOContextProps = [SocketList, (state: object) => void];
 
-export const SocketIOProvider = ({ children }: SocketIOProviderProps) => <SocketIOContext.Provider value={{} as SocketList}>{children}</SocketIOContext.Provider>;
+const SocketIOContext = React.createContext<SocketIOContextProps | null>(null);
+
+export const SocketIOProvider = ({ children }: SocketIOProviderProps) => {
+	const [sockets, setSockets] = useState({});
+	console.log('SOCKETS: ', sockets);
+
+	return <SocketIOContext.Provider value={[sockets, setSockets]}>{children}</SocketIOContext.Provider>;
+};
 
 interface useSocketIOResult {
-	socket: SocketIOClient.Socket;
+	socket: SocketIOClient.Socket | null;
 	connectServer: (uri: string, options: SocketIOClient.ConnectOpts) => void;
+	disconnectServer: () => void;
 }
 
 export const useSocketIO = (name: string): useSocketIOResult => {
-	const sockets = useContext(SocketIOContext) as SocketList;
-	const socket = useMemo(() => sockets[name], [sockets, name]);
+	const [sockets, setSockets] = useContext(SocketIOContext) as SocketIOContextProps;
 
-	const [, socketChange] = useState(false);
+	const socket = sockets[name];
+
+	const closeSocket = useCallback(() => {
+		if (socket) {
+			socket.disconnect();
+			socket.close();
+		}
+	}, [socket]);
 
 	const openSocket = useCallback(
 		(uri: string, options: any = {}) => {
+			if (socket) return socket;
+
 			const defaultOptions = {
 				reconnection: true,
 				reconnectionAttempts: Infinity,
@@ -37,28 +54,24 @@ export const useSocketIO = (name: string): useSocketIOResult => {
 			};
 			const mergeOptions = { ...defaultOptions, ...options };
 
-			// Remove old socket
-			if (socket) {
-				socket.disconnect();
-				socket.close();
-			}
-
 			// New socket
 			const newSocket = io.connect(uri, mergeOptions);
 
 			// Add to list
-			sockets[name] = newSocket;
-			socketChange(v => !v);
+			setSockets((s: SocketList) => {
+				s[name] = newSocket;
+				return _.merge({}, s);
+			});
 
 			return newSocket;
 		},
-		[name, socket, sockets],
+		[name, setSockets, socket],
 	);
 
 	// Check is connected?
 	if (socket && !socket.connected) socket.connect();
 
-	return { socket, connectServer: openSocket };
+	return { socket, connectServer: openSocket, disconnectServer: closeSocket };
 };
 
 interface useSocketIOEventProps {
